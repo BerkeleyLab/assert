8acfc178778f953e6d6f13feb5dcc78c647a6758
Damian Rouson <damian@sourceryinstitute.org>
2021-08-21 12:09:14 -0700

    feat: assertion utility, example, & fpm build file

---
+++ example/example_assertions.f90
@@ -0,0 +1,10 @@
+program assertion_examples
+  !! Demonstrate the use of assetions
+  use assert_m, only : assert
+  implicit none
+  integer, parameter :: i = 1
+  real, parameter :: x = -1.
+  
+  call assert(i > 0, "i > 1") ! Passes
+  call assert(x > 0, description="x > 0", diagnostic_data=x) ! Fails with output containing diagnostic data
+end program
