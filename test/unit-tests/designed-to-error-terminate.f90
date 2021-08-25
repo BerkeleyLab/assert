7c5a01e9fe6f704e0789c838a185acfe0cc7026e
Damian Rouson <damian@sourceryinstitute.org>
2021-08-24 22:39:53 -0700

    feat: make passing tests quiet
    This commit silences most of the output from the tests that are
    designed to error terminate.

--- test/unit-tests/designed-to-error-terminate.f90
+++ test/unit-tests/designed-to-error-terminate.f90
@@ -6,16 +6,45 @@ program designed_to_error_terminate
   integer exit_status
 
   call execute_command_line( &
-    command = "fpm run --example intentionally_false_assertions", &
+    command = "fpm run --example intentionally_false_assertions > /dev/null 2>&1", &
     wait = .true., &
-    exitstat = exit_status )
+    exitstat = exit_status &
+  )
+    
+  block
+    logical error_termination
 
-  sync all
-  
-  if (exit_status == 0) then
-    print *,new_line(''), "designed-to-error-terminate: Yikes! Normal termination detected.  Who designed this OS?", new_line('')
-  else 
-    print *,new_line(''), "----> designed-to-error-terminate: The unit tests designed to error-terminate do so. <----", new_line('')
-  end if
+    error_termination = exit_status /=0
+
+    call co_all(error_termination)
+
+    if (this_image()==1) then
+
+      if (error_termination) then
+        print *, "----> All tests designed to error-terminate pass.    <----"
+      else 
+        print *, "----> One or more tests designed to error-terminate terminated normally. Yikes! Who designed this OS? <----"
+      end if
+
+    end if
+
+  end block
+
+contains
+
+  pure function both(lhs,rhs) result(lhs_or_rhs)
+    logical, intent(in) :: lhs,rhs
+    logical lhs_or_rhs
+
+    lhs_or_rhs = lhs .and. rhs
+
+  end function
+
+  subroutine co_all(boolean)
+    logical, intent(inout) :: boolean
+
+    call co_reduce(boolean, both)
+
+  end subroutine
 
 end program
