ac484c29b5be83dc5df52c577ed7c14f8dfbeceb
Damian Rouson <rouson@lbl.gov>
2024-08-07 18:45:14 -0500

    build(assert_macro.h): mv to include/ subdir

--- test/test-assert-macro.F90
+++ test/test-assert-macro.F90
@@ -6,12 +6,12 @@ program test_assert_macros
   print *,"The call_assert macro"
 
 #define DEBUG
-#include "../../src/assert/assert_macros.h"
+#include "assert_macros.h"
   call_assert(1==1)
   print *,"  passes on not error-terminating when an assertion expression evaluating to .true. is the only argument"
 
 #undef DEBUG
-#include "../../src/assert/assert_macros.h"
+#include "assert_macros.h"
   call_assert(.false.)
   print *,"  passes on being removed by the preprocessor when DEBUG is undefined" // new_line('')
 
@@ -20,12 +20,12 @@ program test_assert_macros
   print *,"The call_assert_describe macro"
 
 #define DEBUG
-#include "../../src/assert/assert_macros.h"
+#include "assert_macros.h"
   call_assert_describe(.true., ".true.")
   print *,"  passes on not error-terminating when assertion = .true. and a description is present"
 
 #undef DEBUG
-#include "../../src/assert/assert_macros.h"
+#include "assert_macros.h"
   call_assert_describe(.false., "")
   print *,"  passes on being removed by the preprocessor when DEBUG is undefined" // new_line('')
 
@@ -34,12 +34,12 @@ program test_assert_macros
   print *,"The call_assert_diagnose macro"
 
 #define DEBUG
-#include "../../src/assert/assert_macros.h"
+#include "assert_macros.h"
   call_assert_diagnose(.true., ".true.", diagnostic_data=1)
   print *,"  passes on not error-terminating when assertion = .true. and description and diagnostic_data are present"
 
 #undef DEBUG
-#include "../../src/assert/assert_macros.h"
+#include "assert_macros.h"
   call_assert_describe(.false., "")
   print *,"  passes on being removed by the preprocessor when DEBUG is undefined"
 
