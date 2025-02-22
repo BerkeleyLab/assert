d0a8e34aa5d6dbbbe771cccae0193a48851da0e7
Damian Rouson <rouson@lbl.gov>
2025-02-22 15:37:58 -0800

    fix(stringification): reduce clashes,better format
    This commit does the following:
    
    1. Renames the `string_m` module and `string` function to
       `fortran_stringify_integer_m` and `fortran_stringify_integer`,
       respectively, to decrease the likelihood of name clashes with
       user code and to clarify the function's limited purpose.
    2. Renames the `STRINGIFY` macro to `CPP_STRINGIFY_SOURCE` to more
       clarify the difference from `fortran_stringify_integer`.
    3. Changes the `fortran_stringify_integer` string-writing format
       from `*` to `(i0)` for more consistent results across compilers.
       The results are the same for the three tested compilers:
       GCC (`gfortran`), NAG (`nagfor`), and LLVM (`flang-new`).

--- src/assert/fortran_stringify_integer_m.f90
+++ src/assert/fortran_stringify_integer_m.f90
@@ -1,16 +1,16 @@
-module string_m
+module fortran_stringify_integer_m
   implicit none
 
 contains  
 
-   pure function string(number) result(number_as_string)
+  pure function fortran_stringify_integer(number) result(number_as_string)
     integer, intent(in) :: number
     integer, parameter :: max_len=128
     character(len=max_len) :: untrimmed_string
     character(len=:), allocatable :: number_as_string
 
-    write(untrimmed_string, *) number
+    write(untrimmed_string, '(i0)') number
     number_as_string = trim(adjustl(untrimmed_string))
   end function
 
-end module string_m
+end module
