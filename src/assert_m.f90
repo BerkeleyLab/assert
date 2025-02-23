65e36612eca26d3c4d177fae1cb4b18baf2f69a5
Damian Rouson <rouson@lbl.gov>
2025-02-22 16:47:28 -0800

    doc(assert_m): clarify public interface
    This commit adds comments to the assert_m module to clarify the
    purpose of the fortran_stringify_integer function and refer users
    to Julienne for a more broadly useful stringification capability.

--- src/assert_m.f90
+++ src/assert_m.f90
@@ -1,7 +1,15 @@
 module assert_m
-  use intrinsic_array_m, only : intrinsic_array_t
+  !! Public interface
+  use intrinsic_array_m,   only : intrinsic_array_t
   use assert_subroutine_m, only : assert, assert_always
-  use characterizable_m, only : characterizable_t
+  use characterizable_m,   only : characterizable_t
+
+  ! The function below is public only to support automated
+  ! invocation via `assert_macros.h`.  For a more broadly useful
+  ! function to convert numeric data to string format, please
+  ! consider using the functions that can be accessed via the 
+  ! `string_t` generic interface in the Julienne framework at
+  ! https://go.lbl.gov/julienne.
   use fortran_stringify_integer_m, only : fortran_stringify_integer
   implicit none
 end module
