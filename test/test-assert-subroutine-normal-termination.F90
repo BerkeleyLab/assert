c555ab87ef82717464a93da010efabd0585ee8c0
Damian Rouson <rouson@lbl.gov>
2025-06-19 20:02:17 -0700

    fix(test): restrict output to image 1

--- test/test-assert-subroutine-normal-termination.F90
+++ test/test-assert-subroutine-normal-termination.F90
@@ -5,16 +5,26 @@ program test_assert_subroutine_normal_termination
   use assert_m, only : assert
   implicit none
 
-  print *
-  print *,"The assert subroutine"
+#if ASSERT_MULTI_IMAGE
+  if (this_image()==1) then
+#endif
+    print *, new_line(''), "The assert subroutine"
+#if ASSERT_MULTI_IMAGE
+  end if
+  sync all
+#endif
 
   call assert(assertion = .true., description = "3 keyword arguments ")
   call assert(            .true., description = "2 keyword arguments ")
   call assert(            .true.,               "no optional argument")
+
 #if ASSERT_MULTI_IMAGE
-    sync all
-    if (this_image()==1) &
+  sync all
+  if (this_image()==1) then
 #endif
     print *,"  passes on not error-terminating when assertion=.true."
+#if ASSERT_MULTI_IMAGE
+  end if
+#endif
   
 end program
