4509eafdc5cfeaa17eee07bf46c16230d5fb2dda
Dan Bonachea <dobonachea@lbl.gov>
2025-06-30 14:11:46 -0700

    Remove dead code
    fortran_stringify_integer_m in no longer used

--- src/assert_m.f90
+++ src/assert_m.f90
@@ -2,13 +2,5 @@ module assert_m
   !! Public interface
   use assert_subroutine_m ! DO NOT PLACE AN ONLY CLAUSE HERE! 
                           ! All public members of assert_subroutine_m are exported
-
-  ! The function below is public only to support automated
-  ! invocation via `assert_macros.h`.  For a more broadly useful
-  ! function to convert numeric data to string format, please
-  ! consider using the functions that can be accessed via the 
-  ! `string_t` generic interface in the Julienne framework at
-  ! https://go.lbl.gov/julienne.
-  use fortran_stringify_integer_m, only : fortran_stringify_integer
   implicit none
 end module
